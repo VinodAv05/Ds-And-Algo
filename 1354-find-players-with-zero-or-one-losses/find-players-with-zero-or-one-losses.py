d2ad0fd70f2bbb3553c6c9e2f9439f988ec2350c
ItsVinod <vinodav05@gmail.com>
2025-08-17 13:21:06 -0400

    Time: 192 ms (39.95%) | Memory: 58.7 MB (35.11%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def findWinners(self, matches):
+        """
+        :type matches: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        winner_map = {}
+        looser_map = {}
+        winner_list = []
+        looser_list = []
+        
+        for match in matches:
+            if match[0] in winner_map:
+                winner_map[match[0]] +=1
+            else:
+                winner_map[match[0]] =1
+            
+            if match[1] in looser_map:
+                looser_map[match[1]]+=1
+            else:
+                looser_map[match[1]]=1
+        
+        for key in winner_map:
+            if key not in looser_map:
+                winner_list.append(key)
+        winner_list.sort()
+        
+        for key, value in looser_map.items():
+            if value ==1:
+                looser_list.append(key)
+        looser_list.sort()    
+        
+        return [winner_list,looser_list]
