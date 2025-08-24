d79fa682f187c101fd5f248ebd19584f9cfb31d8
ItsVinod <vinodav05@gmail.com>
2025-08-24 19:57:25 -0400

    Time: 61 ms (5.00%) | Memory: 18.4 MB (14.01%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransom_map = {}
+        magazine_map = {}
+        check_valid = False
+        
+        for s in "".join(sorted(ransomNote)):
+            if s in ransom_map:
+                ransom_map[s]+=1
+            else:
+                ransom_map[s]=1
+        
+        for k in "".join(sorted(magazine)):
+            if k in magazine_map:
+                magazine_map[k]+=1
+            else:
+                magazine_map[k]=1
+        print("Ransom Map=>",ransom_map)
+        print("Magzine Map=>",magazine_map)
+        for key in ransom_map.keys():
+            print("Ran Key=>",key)
+            if key in magazine_map:
+                if magazine_map[key]>=ransom_map[key]:
+                    check_valid = True
+                else:
+                    check_valid = False
+                    break
+                    
+            else:
+                check_valid = False
+                break
+        
+        return check_valid
+                
+                
+            
+            
+        
