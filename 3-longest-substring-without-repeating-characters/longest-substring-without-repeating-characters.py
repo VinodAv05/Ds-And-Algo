3b552bb6d8d2d1795a806bd2ac284c572daaa683
ItsVinod <vinodav05@gmail.com>
2025-08-26 02:31:09 -0400

    Time: 55 ms (13.82%) | Memory: 31.3 MB (12.12%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        test_var = "abcabcdb"
+        counter = 0
+        holding_map={0:''}
+        tmp_str=''
+        
+        for i in range(len(s)):
+            if s[i] in holding_map[counter]:
+                counter+=1
+                prev_ss = holding_map[counter-1][holding_map[counter-1].index(s[i])+1:]
+                print("prev_ss",prev_ss)
+                holding_map[counter] = prev_ss + s[i]
+            else:
+                holding_map[counter]+=s[i]
+                
+        print("Inside outer else Holding Map=>",holding_map.values())
+        values_len = [len(val) for val in holding_map.values()]
+        print("Inside outer else Holding Map Len=>",values_len)
+        return max(values_len)
+        
+                
+            
+            
+            
+        
+                
+        
