76a7425ce4e6696141f7be018822484ec54b4ff2
ItsVinod <vinodav05@gmail.com>
2025-08-26 02:39:50 -0400

    Time: 22 ms (31.37%) | Memory: 24.3 MB (12.12%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,8 +1,7 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         counter = 0
-        holding_map={0:''}
-        max_s_len=0        
+        holding_map={0:''}   
         for i in range(len(s)):
             if s[i] in holding_map[counter]:
                 counter+=1
@@ -11,12 +10,8 @@ class Solution:
             else:
                 holding_map[counter]+=s[i]
                 
-        #values_len = [len(val) for val in holding_map.values()]
-        
-        for key in holding_map:
-            max_s_len = max(max_s_len, len(holding_map[key]))
-
-        return max_s_len
+        values_len = [len(val) for val in holding_map.values()]
+        return max(values_len)
         
                 
             
