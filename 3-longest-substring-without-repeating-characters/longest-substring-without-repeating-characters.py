c8a7e6b6ec35719fd2170c44e7169ba782632067
ItsVinod <vinodav05@gmail.com>
2025-08-26 02:31:37 -0400

    Time: 39 ms (16.01%) | Memory: 31.3 MB (12.12%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -8,7 +8,6 @@ class Solution:
             if s[i] in holding_map[counter]:
                 counter+=1
                 prev_ss = holding_map[counter-1][holding_map[counter-1].index(s[i])+1:]
-                print("prev_ss",prev_ss)
                 holding_map[counter] = prev_ss + s[i]
             else:
                 holding_map[counter]+=s[i]
